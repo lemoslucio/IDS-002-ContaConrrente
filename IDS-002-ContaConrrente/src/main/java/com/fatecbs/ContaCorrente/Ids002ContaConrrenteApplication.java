c89aabd79d6b0d0a7ac1850100a6bf0be1767eb6
lab3.4 <lab34@fatec.com>
2023-11-09 22:00:40 -0300

    Finaliza Exercicio

---
+++ IDS-002-ContaConrrente/src/main/java/com/fatecbs/ContaCorrente/Ids002ContaConrrenteApplication.java
@@ -0,0 +1,13 @@
+package com.fatecbs.ContaCorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ids002ContaConrrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ids002ContaConrrenteApplication.class, args);
+	}
+
+}
---
+++ IDS-002-ContaConrrente/src/test/java/com/fatecbs/ContaCorrente/Ids002ContaConrrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecbs.ContaCorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ids002ContaConrrenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
